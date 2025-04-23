78a9f7f79d40f5a5fc79cbfce3812b182b793955
jddrtn <jadeedarton@gmail.com>
2025-04-23 19:20:41 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.querySelector(".search-form input");
+    const cards = document.querySelectorAll(".card");
+  
+    searchInput.addEventListener("keyup", function () {
+      const query = this.value.toLowerCase();
+      cards.forEach(card => {
+        const title = card.querySelector(".card-title").textContent.toLowerCase();
+        const text = card.querySelector(".card-text").textContent.toLowerCase();
+        const match = title.includes(query) || text.includes(query);
+        card.parentElement.style.display = match ? "block" : "none";
+      });
+    });
+  });
+  
